ebdc6c45dab5b6c0e2e0855fad8c6de357d9fbce
VChera <vovac8908@gmail.com>
2024-05-13 01:54:52 +0500

    Add CHM file into main menu

---
+++ Assets/Code/MainMenu/Help.cs
@@ -0,0 +1,22 @@
+
+using UnityEngine;
+
+public class Help : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClickHelp()
+    {
+        Applicaion.OpenUrl();
+    }
+}
