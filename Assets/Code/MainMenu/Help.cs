16dba16085857623c33946d30ce14557b7e48902
VChera <vovac8908@gmail.com>
2024-05-13 03:09:39 +0500

    Fix button

--- Assets/Code/MainMenu/Help.cs
+++ Assets/Code/MainMenu/Help.cs
@@ -16,7 +16,7 @@ public class Help : MonoBehaviour
 
     public void OnClick()
     {
-        Application.OpenURL("file:./Assets/Help/FQU.chm");
+        Application.OpenURL("https://docs.google.com/document/d/1uMaF01aMjymAaV12Um5P0WS4f4zoJB1p-7vfp-l2ZRw/edit#bookmark=id.gjdgxs");
         print("click!");
     }
 }
