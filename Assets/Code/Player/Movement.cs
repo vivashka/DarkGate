369cfa8e9a35638782d5d0204b66b59f372ca058
VCher_A <vovac8098@gmail.com>
2024-05-19 12:01:47 +0500

    add animation

--- Assets/Code/Player/Movement.cs
+++ Assets/Code/Player/Movement.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Скорость движения персонажа
+    public float moveSpeed = 2f; // Скорость движения персонажа
+
+    public Animator animator;
 
     public Rigidbody2D rb;
 
@@ -16,13 +19,27 @@ public class Movement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        // Создание вектора движения
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+
+
+        if (movement.sqrMagnitude > 0)
+        {
+            animator.SetFloat("LastH", movement.x);
+            animator.SetFloat("LastV", movement.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            moveSpeed *= 1.5f;
+        }
         
     }
 
     private void FixedUpdate()
     {
-
         // Применение движения к transform
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
