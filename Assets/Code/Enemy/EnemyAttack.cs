d24c347bf8fbef302f4ab5f8e5067f31fbe3af87
VCher_A <vovac8098@gmail.com>
2024-06-19 17:13:44 +0500

    Fix enemy attack

--- Assets/Code/Enemy/EnemyAttack.cs
+++ Assets/Code/Enemy/EnemyAttack.cs
@@ -1,58 +1,58 @@
 using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
     public SmartMove enemyMove;
-    public int attackrange;
+    public int attackRange;
     public GameObject attackBox;
     public Animator animator;
     public int damage = 2;
 
-    public float attackRate = 3;
-    public float attackTimer = 0;
-
     private Rigidbody2D rb;
+    private bool isAttacking = false; // Флаг для контроля выполнения атаки
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void LateUpdate()
     {
-        attackTimer -= Time.deltaTime;
-
-        if (enemyMove.distanceToPlayer < attackrange)
+        if (!isAttacking && enemyMove.distanceToPlayer < attackRange)
         {
-            if (attackTimer <= 0)
-            {
-                Attack();
-            }
-            else
-            {
-                attackTimer = attackRate;
-            }
-            
-        }
-        else
-        {
-            attackBox.SetActive(false);
-            rb.velocity = Vector2.zero;
-            enemyMove.moveSpeed = 3;
+            StartCoroutine(Attack());
         }
     }
 
-    void Attack()
+    IEnumerator Attack()
     {
-        enemyMove.moveSpeed = 0;
-        attackBox.SetActive(true);
-        attackBox.transform.position = transform.position +
-            new Vector3(animator.GetFloat("Horizontal"), animator.GetFloat("Vertical"), 0);
+        isAttacking = true;
 
+        // Остановка перед рывком
+        enemyMove.moveSpeed = 0;
+        rb.velocity = Vector2.zero;
+        yield return new WaitForSeconds(0.5f); // Полсекунды перед рывком
+
+        // Рывок в сторону цели
+        Vector2 attackDirection = ((Vector2)enemyMove.target.position - (Vector2)transform.position).normalized;
+        rb.AddForce(attackDirection * 500); // Настройте силу рывка по вашему усмотрению
+
+        // Полная остановка после рывка
+        rb.velocity = Vector2.zero;
+
+        // Атака
+        attackBox.SetActive(true);
+        attackBox.transform.position = transform.position + new Vector3(attackDirection.x, attackDirection.y, 0);
         animator.SetTrigger("IsAttack");
-        rb.AddForce(new Vector2(animator.GetFloat("Horizontal"), animator.GetFloat("Vertical")) * 20);
+        yield return new WaitForSeconds(0.1f); // Время атаки
+
+        // Остановка после атаки
+        attackBox.SetActive(false);
+        rb.velocity = Vector2.zero;
+        yield return new WaitForSeconds(1); // Задержка после атаки
+
+        isAttacking = false;
+        enemyMove.moveSpeed = 3;
     }
 }
